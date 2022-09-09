e40745f0bf5d074ad44dab6637c7ab4395094964
amanchaudhary01 <aman.patel7474@gmail.com>
2022-09-09 22:39:01 +0530

    almost finished

--- code/script.js
+++ code/script.js
@@ -1,16 +1,29 @@
-console.log("hello");
+
 $(document).ready(function(){
     $("#testimonial-slider").owlCarousel({
         items:3,
         itemsDesktop:[1000,2],
         itemsDesktopSmall:[979,2],
-        itemsTablet:[768,2],
-        itemsMobile:[650,1],
-        pagination:true,
-        navigation:true,
-        navigationText:["",""],
-        autoPlay:true,
-   
+        itemsTablet:[650,1],
+        pagination: true,
+        autoPlay:true
     });
 });
 
+
+  AOS.init();
+  var app = document.getElementById('hero-text-type');
+
+  var typewriter = new Typewriter(app, {
+      loop: true
+  });
+  
+  typewriter.typeString('Doubts')
+      .pauseFor(1500)
+      .deleteAll()
+      .typeString('Needs')
+      .pauseFor(1500)
+      .deleteAll()
+      .typeString('Query')
+      .pauseFor(1500)
+      .start();
